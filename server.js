e537b9b5d0190e6ae2ca96a104390a4e7fa63461
Gabriel Caires <gcaires@thoughtworks.com>
2016-12-14 16:47:42 -0300

    Arregla formateo

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ server.use(express.static( __dirname + '/dist'));
 server.set('view engine', 'ejs');
 
 server.get('/', function(req, res) {
-    res.render('index', { title: 'Yo soy un titulo', content: '<h1>Yo soy una pantalla en blanco o/</h1>' });
+    res.render('index', {
+       title: 'Yo soy un titulo',
+       content: '<h1>Yo soy una pantalla en blanco </h1>'
+     });
 });
 
 server.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 });
