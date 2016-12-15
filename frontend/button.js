68d70ea9e5843d25c1fb1943577f5d22c50df925
Gabriel Caires <gcaires@thoughtworks.com>
2016-12-15 11:47:41 -0300

    Agrega apresentacion

--- frontend/button.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-
-const click = () =>
-      alert('hola');
-
-const Button = () =>
-      <button onClick={click}>Hola</button>
-
-module.exports = Button
--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const App = require('./app');
-
-ReactDOM.render(
-    App({ message: 'Ahora si estoy listo!' }),
-    document.getElementById('content')
-);
